ea7a6dd42b154ff2126518b97855c618cec172b6
Qiu Kun <qiukun11@sina.com>
2016-09-20 23:41:37 -0700

    modified executed, but data analysis is not completed

--- concurrent_level/execute.sh
+++ concurrent_level/execute.sh
@@ -8,9 +8,21 @@ export TST_SCRIPT=$TST_ROOT/scripts/concurrent_level
 
 #%R minor page fault,\t%M Max_Rss,\t%P CPU per
 execute_bug_program(){
-		/usr/bin/time -f "%R\t%M\t%P" -o env.log -a java Main $1 $2 	
-		data_dispose.py
+		/usr/bin/time -f "%R\t%M\t%P" -o $TST_SCRIPT/env.log -a java Main $1 $2 	
+		#data_dispose.py
+}
 
+test(){
+for first in 1000
+	do
+			for second in 3
+			do
+					for i in $(seq 20)
+					do execute_bug_program $first $second
+					done
+
+			done
+	done
 }
 main(){
 	for first in 1000 4000 7000 10000 13000 16000 19000 22000
@@ -24,5 +36,7 @@ main(){
 
 			done
 	done
+}
 
 cd $TST_ROOT/source 
+test
